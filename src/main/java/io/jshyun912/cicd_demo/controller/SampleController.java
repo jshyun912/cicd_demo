570de4bc58a200f9f52a8b93803409a7655a6a32
jshyun912 <dev.hyunjaesung@gmail.com>
2026-06-12 10:08:54 +0900

    init

---
+++ src/main/java/io/jshyun912/cicd_demo/controller/SampleController.java
@@ -0,0 +1,14 @@
+package io.jshyun912.cicd_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SampleController {
+	@GetMapping("/hello")
+	public String HelloWorld() {
+		return "hello";
+	}
+}
